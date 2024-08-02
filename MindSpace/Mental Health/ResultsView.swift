dc12c61f1671f734b80790bbe870eff7bdf47662
Sehaj Dhingra <sehajdhingra2108@gmail.com>
2024-08-02 10:00:26 +0530

    Results view text added. Add video now.

--- MindSpace/Mental Health/ResultsView.swift
+++ MindSpace/Mental Health/ResultsView.swift
@@ -23,11 +23,12 @@ struct ResultsView: View {
         let anxietyCount = responses.filter { $0 == 1 }.count
         
         if panicCount > 1 {
-            return "You might be experiencing symptoms of a panic attack. Consider seeking professional help."
+            return "You might be experiencing symptoms of a panic attack. Consider seeking professional help. Take a quick breather and follow along this video"
+            
         } else if anxietyCount > 1 {
-            return "You might be experiencing anxiety. It may be helpful to explore stress management techniques or consult a professional."
+            return "You might be experiencing anxiety. It may be helpful to explore stress management techniques or consult a professional. Follow along these steps to calm yourself down"
         } else {
-            return "Your responses suggest that you are managing well, but if you experience any troubling symptoms, consider seeking support."
+            return "Your responses suggest that you are managing well, but if you experience any troubling symptoms, consider seeking support. Anyways here is a quick video for you to follow."
         }
     }
 }
