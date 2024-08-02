522e7cd0ade2364d832d3b561c7329b53e2125c3
Sehaj Dhingra <sehajdhingra2108@gmail.com>
2024-08-02 08:27:10 +0530

    Initial Commit

---
+++ MindSpace/MindSpaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindSpaceApp.swift
+//  MindSpace
+//
+//  Created by Umang Dhingra on 8/2/24.
+//
+
+import SwiftUI
+
+@main
+struct MindSpaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
